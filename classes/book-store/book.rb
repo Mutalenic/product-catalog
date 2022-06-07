38403dee1b77b0935f118a129aefffd00bbf66e1
samiullahbahadur <bahadursamiullah@gmail.com>
2022-06-07 16:16:14 +0430

    Fix linters

--- classes/book-store/book.rb
+++ classes/book-store/book.rb
@@ -1,13 +1,11 @@
-
-
 class Book
   attr_accessor :publisher, :cover_state, :label_id, :id
 
   def initialize(publisher, cover_state)
     super()
-    @id=Random.rand(1...10_000)
-    @publisher=publisher
-    @cover_state=cover_state.to_s
-    @label_id=''
+    @id = Random.rand(1...10_000)
+    @publisher = publisher
+    @cover_state = cover_state.to_s
+    @label_id = ''
   end
 end
