3429bfa7550507c35a9d5bd4c07aa27d1eb6da8a
samiullahbahadur <bahadursamiullah@gmail.com>
2022-06-07 21:51:52 +0430

    Created items table

--- DB/schema.sql
+++ DB/schema.sql
@@ -22,3 +22,14 @@ CREATE TABLE authors (
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL
 );
+CREATE TABLE items(
+    id SERIAL PRIMARY KEY,
+    archived BOOLEAN DEFAULT 'f',
+    author_id SERIAL NOT NULL,
+    label_id SERIAL NOT NULL,
+    genre_id SERIAL NOT NULL,
+    publish_date TIMESTAMP NOT NULL,
+    CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES authors(id),
+    CONSTRAINT label_fk FOREIGN KEY (label_id) REFERENCES labels(id),
+    CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES genres(id)
+);
