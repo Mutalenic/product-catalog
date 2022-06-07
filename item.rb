5444913f8a5cc93511766ed4ace55bfbddbb5dd5
Nicholas Mutale <nicomutale@gmail.com>
2022-06-07 16:31:54 +0200

    Add methods

--- item.rb
+++ item.rb
@@ -6,6 +6,26 @@ class Item
         @archived = archived
         @id = rand(1..100)
     end
+    def author=(author)
+        @author = author
+        @author.items << self unless @author.items.include?(self)
+    end
+
+    def genre=(genre)
+        @genre = genre
+        @genre.items << self unless @genre.items.include?(self)
+    end
+
+    def source=(source)
+        @source = source
+        @source.items << self unless @source.items.include?(self)
+    end
+
+    def label=(label)
+        @label = label
+        @label.items << self unless @label.items.include?(self)
+    end
+   
 end
 
     
