1d7bdb8108dc00e36b8fffd839c572aca878cab6
Nicholas Mutale <nicomutale@gmail.com>
2022-06-07 16:43:34 +0200

    Fix linter errors

--- item.rb
+++ item.rb
@@ -1,39 +1,38 @@
 class Item
-    attr_accessor :publish_date :archived
-    attr_reader :author, :genre, :source, :label, :id
-    def initialize(publish_date, archived: false)
-        @publish_date = publish_date
-        @archived = archived
-        @id = rand(1..100)
-    end
-    def author=(author)
-        @author = author
-        @author.items << self unless @author.items.include?(self)
-    end
+  attr_accessor :publish_date
+  attr_reader :archived, :id, :genre, :label, :author, :source
 
-    def genre=(genre)
-        @genre = genre
-        @genre.items << self unless @genre.items.include?(self)
-    end
+  def initialize(publish_date, archived: false)
+    @publish_date = publish_date
+    @archived = archived
+    @id = rand(1..100)
+  end
 
-    def source=(source)
-        @source = source
-        @source.items << self unless @source.items.include?(self)
-    end
+  def author=(author)
+    @author = author
+    @author.items << self unless @author.items.include?(self)
+  end
 
-    def label=(label)
-        @label = label
-        @label.items << self unless @label.items.include?(self)
-    end
-   
-    def can_be_archived?
-        @publish_date.year < Time.now.year - 10
-    end
+  def genre=(genre)
+    @genre = genre
+    @genre.items << self unless @genre.items.include?(self)
+  end
 
-    def move_to_archive
-        @archived = true if can_be_archived?
-    end
-   
+  def source=(source)
+    @source = source
+    @source.items << self unless @source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    @label.items << self unless @label.items.include?(self)
+  end
+
+  def can_be_archived?
+    @publish_date.year < Time.now.year - 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
-
-    
