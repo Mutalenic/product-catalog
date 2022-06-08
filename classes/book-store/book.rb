cb52839d963c4e25d2b441a42316181c9407a88f
samiullahbahadur <bahadursamiullah@gmail.com>
2022-06-08 16:02:24 +0430

    Fix rspec error

--- classes/book-store/book.rb
+++ classes/book-store/book.rb
@@ -5,7 +5,7 @@ class Book < Item
 
   def initialize(publisher, cover_state)
     super()
-    @id = Random.rand(1...10_000)
+    @id = Random.rand(1..10_000)
     @publisher = publisher
     @cover_state = cover_state.to_s
     @label_id = ''
